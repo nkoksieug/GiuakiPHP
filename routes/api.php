69137a13a6141579a87434146fe1f14c0c71b26d
nkoksieug <dat0386535512@gmail.com>
2025-12-17 14:47:21 +0700

    Sua code route va controller

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\ApiTaskController;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+use App\Http\Controllers\ResourceTaskController;
 
-
+Route::get('tasks/create', [ResourceTaskController::class, 'create']);
 Route::apiResource('tasks', ApiTaskController::class)->names('api.tasks');
