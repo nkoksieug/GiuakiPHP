e9956438140abc7cf98f0678fd6954f52029fe38
nkoksieug <dat0386535512@gmail.com>
2025-12-17 21:27:28 +0700

    Cau hinh SSL cho TiDB

--- config/database.php
+++ config/database.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => env('DB_CONNECTION', 'mysql'), // Đã đổi mặc định thành mysql cho tiện
 
     /*
     |--------------------------------------------------------------------------
@@ -58,9 +58,11 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                (PHP_VERSION_ID >= 80500 ? \Pdo\Mysql::ATTR_SSL_CA : \PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+            // --- ĐOẠN ĐÃ SỬA: Bắt buộc dùng SSL nếu có cấu hình ---
+            'options' => array_filter([
+                \PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]),
+            // -----------------------------------------------------
         ],
 
         'mariadb' => [
